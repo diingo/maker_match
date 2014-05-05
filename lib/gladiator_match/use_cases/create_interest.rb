51a0848aef3013d8eb8090e1cae6ae7ca7be41da
Gene Le <gene.gkl@gmail.com>
2014-05-05 10:29:25 -0500

    add create interest test and use case

--- lib/gladiator_match/use_cases/create_interest.rb
+++ lib/gladiator_match/use_cases/create_interest.rb
@@ -1,4 +1,17 @@
 module GladiatorMatch
   class CreateInterest < UseCase
+    def run(inputs)
+      
+      return failure(:invalid_expertise) unless inputs[:expertise].match /^beginner|intermediate|advanced$/
+
+      return failure(:invalid_name) unless valid_name?(inputs[:name])
+
+      interest = GladiatorMatch.db.create_interest(expertise: inputs[:expertise], name: inputs[:name])
+      success(interest: interest)
+    end
+
+    def valid_name?(name)
+      name.nil? == false && name.empty? == false
+    end
   end
 end
--- spec/use_cases/create_interest_spec.rb
+++ spec/use_cases/create_interest_spec.rb
@@ -4,11 +4,36 @@ describe GladiatorMatch::CreateInterest do
 
   let(:result) { described_class.run(@params) }
 
-  context 'error handling' do
-    it 'ensures expertise is either beginner, intermediate, or advanced'
-
-    it 'ensures name is not blank'
+  before do
+    @params = { expertise: 'beginner', name: 'haskell'}
   end
 
-  it 'creates an interest'
+  context 'error handling' do
+    it 'ensures expertise is either beginner, intermediate, or advanced' do
+      @params[:expertise] = 'awesome'
+
+      expect(result.success?).to eq(false)
+      expect(result.error).to eq(:invalid_expertise)
+    end
+
+    it 'ensures name is not nil or blank' do
+      @params[:name] = nil
+      result = described_class.run(@params)
+
+      expect(result.success?).to eq(false)
+      expect(result.error).to eq(:invalid_name)
+
+      @params[:name] = ''
+      result = described_class.run(@params)
+      expect(result.success?).to eq(false)
+      expect(result.error).to eq(:invalid_name)
+    end
+  end
+
+  it 'creates an interest' do
+    expect(result.success?).to eq(true)
+
+    expect(result.interest.name).to eq('haskell')
+    expect(result.interest.expertise).to eq('beginner')
+  end
 end
