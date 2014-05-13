ed993f54f16c47ede377ef69da65c49c0f8c61e3
Gene Le <gene.gkl@gmail.com>
2014-05-13 10:44:06 -0500

    track file for log in use case

---
+++ spec/use_cases/log_in_spec.rb
@@ -0,0 +1,39 @@
+require_relative '../spec_helper.rb'
+
+describe GladiatorMatch::LogIn do
+  # let(:interest_1) { GladiatorMatch.db.create_interest(name: 'haskell', expertise: 'advanced')}
+  let(:peach) {
+    GladiatorMatch.db.create_user(
+      :first_name => 'Peach',
+      :last_name => 'Peach',
+      :email => 'peach@example.com',
+      :github_login => 'peach',
+      :interests => [interest_1],
+      :password => '123'
+    )
+  }
+
+  let(:result) { described_class.run(@params) }
+
+  before do
+    @params = { github_login: peach.github_login }
+  end
+
+  context "failure" do
+    it "errors if login is invalid - empty string or nil" do
+      @params[:github_login] = ''
+
+      expect(result.success?).to eq(false)
+      expect(result.error).to eq(:invalid_login)
+    end
+
+  end
+
+  context "success" do
+    it 'creates a session - no user id yet (that will be added to the session later after we run create user use case)' do
+      expect(result.success?).to eq(true)
+      expect(result.session)
+    end
+  end
+
+end
