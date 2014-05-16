f3d932bee4a31226d48ab90228d99cc1a32112f1
Gene Le <gene.gkl@gmail.com>
2014-05-16 10:16:44 -0500

    temp add all

--- web/app/controllers/sessions_controller.rb
+++ web/app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
-    # binding.pry
   def create
+    binding.pry
+    # auth_hash["extra"]["raw_info"]["login"] is where the login is located
     @user = User.find_or_create_from_auth_hash(auth_hash)
     self.current_user = @user
     redirect_to '/'
